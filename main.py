808b4bc90988df69a4e50cd4abb3f7dca2ab2ba4
Victor Senna <vhsenna@gmail.com>
2022-05-24 18:35:14 -0300

    CRUD

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import schemas
 
 app = FastAPI()
 
@@ -15,3 +16,19 @@ def get_items():
 @app.get('/{id}')
 def get_item(id:int):
     return fake_database[id]
+
+@app.post('/')
+def add_item(item:schemas.Item):
+    new_id = len(fake_database.keys()) + 1
+    fake_database[new_id] = {'task': item.task}
+    return fake_database
+
+@app.put('/{id}')
+def update_item(id:int, item:schemas.Item):
+    fake_database[id]['task'] = item.task
+    return fake_database
+
+@app.delete('/{id}')
+def delete_item(id:int):
+    del fake_database[id]
+    return fake_database
